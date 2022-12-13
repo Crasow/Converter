a86fdf1061e2ca72f0511fabb333c5eb38832871
Crasow <olariliya2003@gmail.com>
2022-12-13 22:42:22 +0200

    init

---
+++ blankChlen/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using blankChlen.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace blankChlen.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ blankChlen/Views/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using blankChlen.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace blankChlen.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+        Label header;
+        //Xamarin.Forms.Picker picker;
+
+        public void MainPage()
+        {
+            header = new Label
+            {
+                Text = "Выберите язык",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            };
+
+            //Xamarin.Forms.Picker picker = new Xamarin.Forms.Picker
+            picker.Items.Add("Ярд");
+            picker.Items.Add("КилоЯрд");
+            picker.Items.Add("Футы");
+            picker.Items.Add("М");
+            picker.Items.Add("Км");
+
+
+            //this.Content = new StackLayout { Children = { header, picker } };
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Debug.Text = "Вы выбрали: " + picker.Items[picker.SelectedIndex];
+        }
+    }
+}
